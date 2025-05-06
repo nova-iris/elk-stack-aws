bf7a64621e617a9291a7f55421646ec1213866b2
Trong Truong <trongtruong2509@gmail.com>
2025-05-06 10:56:30 +0700

    complete terraform provision

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,34 @@
-# This file has been split into separate files:
-# - versions.tf - Terraform configuration and AWS provider
-# - vpc.tf - VPC module configuration
-# - security_groups.tf - Security groups for Elasticsearch
-# - elasticsearch.tf - Elasticsearch instances and SSH key pair
-# - data.tf - Data sources for AMI lookup
+# Latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Generate Ansible inventory file
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/templates/inventory.tftpl", {
+    elasticsearch_instances = aws_instance.elasticsearch,
+    logstash_instance       = aws_instance.logstash,
+    filebeat_instance       = aws_instance.filebeat,
+    cluster_name            = var.cluster_name,
+    elasticsearch_version   = var.elasticsearch_version,
+    enable_ui               = var.enable_ui
+  })
+  filename = "${path.module}/../ansible/inventory/elk.ini"
+
+  depends_on = [
+    aws_instance.elasticsearch,
+    aws_instance.logstash,
+    aws_instance.filebeat
+  ]
+}
